62e1b9bea23dd209853e0da0a2ec7a23588d4a7d
SimonIronEggs <1367983313@qq.com>
2020-01-11 18:01:22 +0800

    我的和目的地完成

--- pages/personal/personal.js
+++ pages/personal/personal.js
@@ -1,15 +1,14 @@
 // pages/personal/personal.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    login:'登录/注册'
   },
+  //进入设置
   onClickSet(){
-    console.log(1);
-    
     wx.navigateTo({
       url: '/pages/settings/settings',
       success: (result)=>{
@@ -19,71 +18,24 @@ Page({
       complete: ()=>{}
     });
   },
+  //进入反馈
   onClickFeeback(){
-    console.log(1);
-    
     wx.navigateTo({
       url: '/pages/feedback/feedback',
       success: (result)=>{
-        
       },
       fail: ()=>{},
       complete: ()=>{}
     });
   },
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //进入登录
+  bindGetUserInfo(){
+    wx.navigateTo({
+      url: '/pages/login/login',
+      success: (result)=>{
+      },
+      fail: ()=>{},
+      complete: ()=>{}
+    });
   }
 })
