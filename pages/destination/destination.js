bda7149450193eafd300f571beaaa5a34f504f00
SimonIronEggs <1367983313@qq.com>
2020-01-13 11:52:39 +0800

    1/3/11:52

--- pages/destination/destination.js
+++ pages/destination/destination.js
@@ -1,73 +1,9 @@
 // pages/destination/destination.js
 Page({
-<<<<<<< HEAD
-
-=======
->>>>>>> Duke
   /**
    * 页面的初始数据
    */
   data: {
-<<<<<<< HEAD
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-=======
     items: [], //目的地左侧列表text
     itemslist: [], //目的地左侧列表其他数据
     destinationList: [], //目的地右侧矩阵
@@ -184,5 +120,4 @@ Page({
       }
     })
   },
->>>>>>> Duke
 })
